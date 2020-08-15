be7d7513cfa63f3b4f161c3e5e6d5f0227d619da
oldtimerza <bradleymorrisza@gmail.com>
2020-08-15 18:02:47 +0200

    Rename assets folder and add texture, refactor geometry to make more sense

--- TinyRenderer/geometry.cpp
+++ TinyRenderer/geometry.cpp
@@ -54,6 +54,12 @@ void line(Vec2f v0, Vec2f v1, Buffer &buffer, Color color)
 
 Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) 
 {
+    //Checking if the point P lies on a triangle involves checking if the u,v coords on the non-orthoganl basis created 
+    //by basis vectors: C-A and B-A from origin A, i.e A + u(C-A) + v(B-A) = P. After some simplification it requires that
+    //(1-u-v)A+vB+uC = P => (A-P) + u(C - A) + v(B - A) = 0(the zero vector) which requires both x,y (screen co-ords) are 0. After some solving
+    //we require [u v 1][(B-A)x (C-A)x (A-P)x]^T = 0, [u v 1][(B-A)y (C-A)y (A-P)y]^T = 0. In other words to solve for u,v we just need to find 
+    //a vector that is a cross product of s0 = [(B-A)x (C-A)x (A-P)x] and s1 = [(B-A)y (C-A)y (A-P)y], since the cross product will be orthoganal to both
+    //i.e the dot product of [u v 1] with s0 and s1 will be 0. Once we have this cross product vector then we can return the co-ords for P: [ 1-u-v, v,u].
     Vec3f s[2];
     for (int i = 2; i--; ) 
     {
@@ -61,10 +67,14 @@ Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P)
         s[i].y = B.raw[i] - A.raw[i];
         s[i].z = A.raw[i] - P.raw[i];
     }
-    Vec3f u = s[0] ^ s[1];
-    if (std::abs(u.raw[2]) > 1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
-        return Vec3f(1.f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
-    return Vec3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+    Vec3f cross = s[0] ^ s[1];
+    if (std::abs(cross.raw[2]) <= 1e-2) // dont forget that cross.z is an integer. If it is zero(or in error range) then triangle ABC is degenerate
+		return Vec3f(-1, 1, 1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
+    cross = cross * (1.0f / cross.z);// We do this to set  the z value to 1 so we get the form [u v 1]
+    float w = 1.f - (cross.x + cross.y);
+    float v = cross.y;
+    float u = cross.x;
+	return Vec3f(w, v, u);
 };
 
 void triangle(Vec3f *pts, float *zbuffer, Buffer &buffer, Color color)
@@ -90,6 +100,7 @@ void triangle(Vec3f *pts, float *zbuffer, Buffer &buffer, Color color)
             Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
             if (bc_screen.x < 0 || bc_screen.y < 0 || bc_screen.z < 0) continue;
             P.z = 0;
+            //We interpolate the Z values of the 3 points to get the Z value of P on the triangle.
             for (int i = 0; i < 3; i++) P.z += pts[i].z * bc_screen.raw[i];
             if (zbuffer[int(P.x + P.y * width)] < P.z) 
             {
